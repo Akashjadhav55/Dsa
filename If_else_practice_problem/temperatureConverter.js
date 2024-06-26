0a969e7c73f0e2fafce7787ba31c609f29708fe2
Akashjadhav55 <akashjadhav7040253178@gmail.com>
2024-06-26 21:03:32 +0530

    mm

--- If_else_practice_problem/temperatureConverter.js
+++ If_else_practice_problem/temperatureConverter.js
@@ -1,2 +1,14 @@
 // Write a javascript program that converts temperatures between Celsius and Fahrenheit. The user should input the temperature and its unit (C or F), and the program should convert it to the other unit.
 
+let n = [5,1,1,2,2,4]
+let num = +n[0]
+let array = []
+for(let i = 1; i < n.length ; i++){
+    array += n[i]
+  
+}
+console.log(array)
+
+for(let i=0; i<array.length; i++){
+    
+}
