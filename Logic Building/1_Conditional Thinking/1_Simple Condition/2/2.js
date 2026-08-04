3696b30973c0e3e345ee4c9c5110dc460dc62eb5
Akashjadhav55 <Akashjadhav7040253178@gmail.com>
2026-08-04 23:26:34 +0530

    Add function to check if a number is even or odd

--- Logic Building/1_Conditional Thinking/1_Simple Condition/2/2.js
+++ Logic Building/1_Conditional Thinking/1_Simple Condition/2/2.js
@@ -4,3 +4,16 @@
 
 // Write your solution here
 
+
+
+function checkEvenOdd(num) {
+
+    if(num % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+};
+
+console.log(checkEvenOdd(4)); // Output: "Even"
+console.log(checkEvenOdd(7)); // Output: "Odd"
