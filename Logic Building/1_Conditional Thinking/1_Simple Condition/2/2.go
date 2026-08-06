c82b79581adf00693a1d571f93ac17d5c2be6523
Akashjadhav55 <Akashjadhav7040253178@gmail.com>
2026-08-06 06:43:18 +0530

    Refactor number checking logic to improve clarity in 2.go

--- Logic Building/1_Conditional Thinking/1_Simple Condition/2/2.go
+++ Logic Building/1_Conditional Thinking/1_Simple Condition/2/2.go
@@ -11,7 +11,10 @@ func main() {
     var num int
     fmt.Print("Enter a number: ")
     fmt.Scanln(&num)
-
-
+    if num%2 == 0 {
+        fmt.Println("Even")
+    } else {
+        fmt.Println("Odd")
+    }   
     
 }
